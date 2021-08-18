fdbe9578394aca3bf70e7cef0558b118639b059f
saif shaikh <saif12345shaikh@gmail.com>
2021-08-18 17:35:18 +0530

    Diff.java committed

---
+++ Diff.java
@@ -0,0 +1,9 @@
+import javax.print.event.PrintEvent;
+
+public class Diff {
+  public static void main(String[] args) {
+    int a=10;
+    int b=5;
+    System.out.println("The diff is :" + (a-b));
+  }
+}
