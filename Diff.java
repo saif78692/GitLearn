8c042542de29975fe19fb1a1a68457f74aa6557f
saif78692 <saif12345shaikh@gmail.com>
2021-08-19 10:34:24 +0530

    Diff.java commited for 2nd time

--- Diff.java
+++ Diff.java
@@ -4,6 +4,6 @@ public class Diff {
   public static void main(String[] args) {
     int a=10;
     int b=5;
-    System.out.println("The diff is :" + (a-b));
+    System.out.println("The diff between 2 no is  :" + (a-b));
   }
 }
